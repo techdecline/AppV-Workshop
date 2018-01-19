1d128192962df268e575f060fb1d01c7b0462dd6
techdecline <cornelius.schuchardt@gmail.com>
2018-01-19 11:00:17 +0100

    Add scripts

---
+++ Lab4 - Local Troubleshooting.ps1
@@ -0,0 +1,11 @@
+﻿# Start Virtual Bubble
+$appv = Get-AppvClientPackage
+Start-AppvVirtualProcess -FilePath powershell -AppvClientObject $appv
+
+# Repair App-V Package
+
+## Recreate Extensions
+Get-AppvClientPackage | Repair-AppvClientPackage -Extensions
+
+## Reset Virtual Bubble
+Get-AppvClientPackage | Repair-AppvClientPackage -UserState
